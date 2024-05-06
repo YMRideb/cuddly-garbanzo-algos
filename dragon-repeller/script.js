95eaafaccd29f55a00b9f5908ca0edc2c08e5438
Yukio Rideb <103914884+YMRideb@users.noreply.github.com>
2024-05-05 19:17:13 -0700

    Update script.js
    had to access second element of locations array via bracket notation!

--- dragon-repeller/script.js
+++ dragon-repeller/script.js
@@ -37,21 +37,21 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
 }
 
 function goTown() {
-
+  update(locations[0]);
 }
 
 function goStore() {
-
+  update(locations[1]);
 }
 
 function goCave() {
